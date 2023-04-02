8ec67ded7ce469b23ab39a43afbea650f92d98a8
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 07:54:39 +0200

    owo

--- dimitra/js/copy.js
+++ dimitra/js/copy.js
@@ -1,5 +1,24 @@
 let btnSend = document.querySelector('button');
 let message = document.querySelector('h1');
+function copyBase() 
+{                       
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Copied';
+    setTimeout(()=>{
+        btnSend.innerText = 'Copy again?'
+    },3000);
+});
+
+
+
+
 function CopyToClipboard(id)
 {
 var r = document.createRange();
@@ -9,10 +28,3 @@ window.getSelection().addRange(r);
 document.execCommand('copy');
 window.getSelection().removeAllRanges();
 }
-
-btnSend.addEventListener('click', () =>{
-    btnSend.innerText = 'Copied';
-    setTimeout(()=>{
-        btnSend.innerText = 'Click to Copy command'
-    },3000);
-});
