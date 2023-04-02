ccdfa7721fd0c4824ab450dcd4ed1a2b30df2252
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 08:49:32 +0200

    pls work

--- dimitra/js/copy.js
+++ dimitra/js/copy.js
@@ -1,16 +1,16 @@
-let text = document.getElementById('base').innerHTML;
-const copyContent = async () => {
-  try {
-    await navigator.clipboard.writeText(text);
-    console.log('Content copied to clipboard');
-    
-  } catch (err) {
-    console.error('Failed to copy: ', err);
-  }
-  clipboard.on('success', function(e) {
-    $btnCopy.text('Copied');
-
-    setTimeout(function() {
-      $btnCopy.text('Copy');
-    }, 2000);
-})}
+$(function() {
+    var $btnCopy = $('#copy-button');
+  
+    $btnCopy.on('click', function() {
+      var clipboard = new Clipboard('#copy-button');
+  
+      clipboard.on('success', function(e) {
+        $btnCopy.text('Copied');
+  
+        setTimeout(function() {
+          $btnCopy.text('Copy');
+        }, 2000);
+      });
+    });
+  });
+  
