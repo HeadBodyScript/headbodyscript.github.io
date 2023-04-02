de499b07ca71738f9bb83d1c175372b181cdf4a6
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 08:03:06 +0200

    maybe

--- dimitra/js/copy.js
+++ dimitra/js/copy.js
@@ -9,22 +9,13 @@ function copyBase()
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
 }
+then(() => {
 btnSend.addEventListener('click', () =>{
     btnSend.innerText = 'Copied';
     setTimeout(()=>{
         btnSend.innerText = 'Copy again?'
     },3000);
 });
+});
 
 
-
-
-function CopyToClipboard(id)
-{
-var r = document.createRange();
-r.selectNode(document.getElementById(id));
-window.getSelection().removeAllRanges();
-window.getSelection().addRange(r);
-document.execCommand('copy');
-window.getSelection().removeAllRanges();
-}
