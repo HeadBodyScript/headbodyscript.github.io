343536997bde75126076e9c2250a658a14ec3c29
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 06:33:21 +0200

    A fix I hope

--- dimitra/js/pages/index.js
+++ dimitra/js/pages/index.js
@@ -15,20 +15,6 @@ let getCat = () => {
 }
 getCat();
 
-// Copy IP Button
-let btnSend = document.querySelector('button');
-let message = document.querySelector('h1');
-function copyText() {                    
-    navigator.clipboard.writeText
-        ("headbodyscript.ddns.net:25582");
-}
-btnSend.addEventListener('click', () =>{
-    btnSend.innerText = 'Copied';
-    setTimeout(()=>{
-        btnSend.innerText = 'Copy again?'
-    },3000);
-});
-
 // Get YouTube Information
 const youtubeKey = 'AIzaSyD6Twf3IrJNQ3Ffc58IJxUMXxdImkJbQXM';
 const youtubeUser = 'UCqYWhYvSGTLO0L-VIPpywPQ';
