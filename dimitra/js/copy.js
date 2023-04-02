0b12b49f7b2951ac2adf64c3ecaf59778a47db00
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 08:39:54 +0200

    Update copy.js

--- dimitra/js/copy.js
+++ dimitra/js/copy.js
@@ -1,8 +1,12 @@
+let btnSend = document.querySelector('button');
+let message = document.querySelector('h1');
+
 let text = document.getElementById('base').innerHTML;
 const copyContent = async () => {
   try {
     await navigator.clipboard.writeText(text);
     console.log('Content copied to clipboard');
+    btnSend.innerText = 'Copied'
   } catch (err) {
     console.error('Failed to copy: ', err);
   }
