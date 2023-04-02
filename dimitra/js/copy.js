7544f7af1e0420e853398e3f43e533aa7d58bf6f
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 09:10:05 +0200

    k

--- dimitra/js/copy.js
+++ dimitra/js/copy.js
@@ -1,16 +1,18 @@
-$(function() {
-    var $btnCopy = $('#copy-button');
-  
-    $btnCopy.on('click', function() {
-      var clipboard = new Clipboard('#copy-button');
-  
-      clipboard.on('success', function(e) {
-        $btnCopy.text('Copied');
-  
-        setTimeout(function() {
-          $btnCopy.text('Copy');
-        }, 2000);
-      });
-    });
-  });
-  
+let btnSend = document.querySelector('button');
+let message = document.querySelector('h1');
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
+
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Copied';
+    setTimeout(()=>{
+        btnSend.innerText = 'Click to Copy command'
+    },3000);
+});
