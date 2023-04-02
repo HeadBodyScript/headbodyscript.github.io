2fb750467d765559beb28f4b3022c05eb861c4b3
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-02 06:19:26 +0200

    Update sidebar and index js enabled

---
+++ dimitra/js/pages/copy.js
@@ -0,0 +1,19 @@
+
+let btnSend = document.querySelector('button');
+let message = document.querySelector('h1');
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
+
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Copied';
+    setTimeout(()=>{
+        btnSend.innerText = 'Click to Copy command'
+    },3000);
+});
---
+++ dimitra/js/pages/index.js
@@ -0,0 +1,92 @@
+// Get Random Cat Fact
+const catFact = document.getElementById('catFact');
+let getCat = () => {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  fetch("https://meowfacts.herokuapp.com/", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      catFact.innerHTML = result.data;
+  })
+    .catch(error => console.log('error', error));
+}
+getCat();
+
+// Copy IP Button
+let btnSend = document.querySelector('button');
+let message = document.querySelector('h1');
+function copyText() {                    
+    navigator.clipboard.writeText
+        ("headbodyscript.ddns.net:25582");
+}
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Copied';
+    setTimeout(()=>{
+        btnSend.innerText = 'Copy again?'
+    },3000);
+});
+
+// Get YouTube Information
+const youtubeKey = 'AIzaSyD6Twf3IrJNQ3Ffc58IJxUMXxdImkJbQXM';
+const youtubeUser = 'UCqYWhYvSGTLO0L-VIPpywPQ';
+const getYouTubesubs = document.getElementById('getYouTubesubs');
+const getYouTubeviews = document.getElementById('getYouTubeviews');
+let getSubscribers = () => {
+    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+        getYouTubesubs.innerHTML = data["items"][0].statistics.subscriberCount;
+        getYouTubeviews.innerHTML = data["items"][0].statistics.viewCount;
+    })
+      .catch(error => console.log('error', error));
+}
+getSubscribers();
+
+// Get Minecraft Server Status
+const serverStatus = document.getElementById('serverStatus');
+const serverMax = document.getElementById('serverMax');
+const serverOnline = document.getElementById('serverOnline');
+const serverVersion = document.getElementById('serverVersion');
+let getServer = () => {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      fetch("https://eu.mc-api.net/v3/server/ping/headbodyscript.ddns.net:25582", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            serverStatus.innerHTML = result.online;
+            serverMax.innerHTML = result.players.max;
+            serverOnline.innerHTML = result.players.online;
+            serverVersion.innerHTML = result.version.name;
+        })
+        .catch(error => console.log('error', error));
+}
+getServer   ();
+
+// Get Github User Information
+const getGitSubs = document.getElementById('getGitSubs');
+const getGitRepo = document.getElementById('getGitRepo');
+let getInfo = () => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("https://api.github.com/users/headbodyscript", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        getGitSubs.innerHTML = result.followers;
+        getGitRepo.innerHTML = result.public_repos;
+    })
+      .catch(error => console.log('error', error));
+}
+getInfo();
+  
