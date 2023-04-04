6048c29379df02ec73a41a081699d8f69b730335
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-04 09:33:42 +0200

    google analytics

---
+++ google.js
@@ -0,0 +1,11 @@
+document.write(`
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2BCEYTZM4F%22%3E</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2BCEYTZM4F');
+</script>
+`);
