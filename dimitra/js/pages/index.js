548f5a95bf91179585c0440709b21e43943148ae
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-04-04 11:01:14 +0200

    Update index.js

--- dimitra/js/pages/index.js
+++ dimitra/js/pages/index.js
@@ -1,19 +1,3 @@
-const catFact = document.getElementById('catFact');
-let getCat = () => {
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  fetch("https://meowfacts.herokuapp.com/", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      catFact.innerHTML = result.data;
-  })
-    .catch(error => console.log('error', error));
-}
-getCat();
-
 let btnSend = document.querySelector('button');
 let message = document.querySelector('h1');
 function copyText() {                       
