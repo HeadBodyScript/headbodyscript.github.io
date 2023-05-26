d8e0bf839d554f2d5b2714c248c134e3ed3af016
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-05-26 05:55:40 +0200

    Update index.js

--- dimitra/js/pages/index.js
+++ dimitra/js/pages/index.js
@@ -2,7 +2,7 @@ let btnSend = document.querySelector('button');
 let message = document.querySelector('h1');
 function copyText() {                       
     navigator.clipboard.writeText
-        ("headbodyscript.ddns.net:25582");
+        ("45.82.121.87");
 }
 btnSend.addEventListener('click', () =>{
     btnSend.innerText = 'Copied';
@@ -38,7 +38,7 @@ let getServer = () => {
         method: 'GET',
         redirect: 'follow'
       };
-      fetch("https://eu.mc-api.net/v3/server/ping/135.125.24.168:25582", requestOptions)
+      fetch("https://eu.mc-api.net/v3/server/ping/45.82.121.87", requestOptions)
         .then(response => response.json())
         .then(result => {
             console.log(result);
