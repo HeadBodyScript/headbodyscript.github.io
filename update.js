ccd480fec9d54c9c89cd062d6fbf0928457381b1
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-08-13 06:21:51 +0200

    update

--- update.js
+++ update.js
@@ -1,4 +1,12 @@
 document.write(`
-<a>mew 2.2 </a><br/>
-<a>meow 2.2 </a><br/>
+<p>We updated the <a href="/pages/texture/armorstands.html">armorstands</a> texturepack!</p>
+<a>Version 2.2 </a><br/>
+<a>Now supports multiple models (Optifine 1.19+ only) </a><br/>
+<a>Added Alex slim model </a><br/>
+<a>Changed the base armorstand back to vanilla </a><br/>
+<a>Changed skins to the following: </a><br/>
+<a>- Rebecca </a><br/>
+<a>- Lucy</a><br/>
+<a>- Johnny Silverhand</a><br/>
+<a>- David Martinez</a><br/>
 `);
