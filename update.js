278f71ec08adf19c5c93447368029002139b19bb
IHeadBodyScript <IHeadBodyScript@gmail.com>
2023-08-14 09:01:54 +0200

    Update

--- update.js
+++ update.js
@@ -1,12 +1,3 @@
 document.write(`
-<p>We updated the <a href="/pages/texture/armorstands.html">armorstands</a> texturepack!</p>
-<a>Version 2.2 </a><br/>
-<a>Now supports multiple models (Optifine 1.19+ only) </a><br/>
-<a>Added Alex slim model </a><br/>
-<a>Changed the base armorstand back to vanilla </a><br/>
-<a>Changed skins to the following: </a><br/>
-<a>- Rebecca </a><br/>
-<a>- Lucy</a><br/>
-<a>- Johnny Silverhand</a><br/>
-<a>- David Martinez</a><br/>
+<p>Important notes will be written here</p>
 `);
