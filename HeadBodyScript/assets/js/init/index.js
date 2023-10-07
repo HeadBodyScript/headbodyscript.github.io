cf5e541f3636ac9c6503024a913e27d48801caab
IHeadBodyScript <IHeadBodyScript@gmail.com>
2023-10-07 21:42:00 +0200

    Update index.js

--- HeadBodyScript/assets/js/init/index.js
+++ HeadBodyScript/assets/js/init/index.js
@@ -15,7 +15,27 @@ btnSend.addEventListener('click', () =>{
 
 const youtubeKey="AIzaSyD6Twf3IrJNQ3Ffc58IJxUMXxdImkJbQXM",youtubeUser="UCqYWhYvSGTLO0L-VIPpywPQ",getYouTubesubs=document.getElementById("getYouTubesubs"),getYouTubeviews=document.getElementById("getYouTubeviews");let getSubscribers=()=>{fetch("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCqYWhYvSGTLO0L-VIPpywPQ&key=AIzaSyD6Twf3IrJNQ3Ffc58IJxUMXxdImkJbQXM").then(e=>e.json()).then(e=>{console.log(e),getYouTubesubs.innerHTML=e.items[0].statistics.subscriberCount,getYouTubeviews.innerHTML=e.items[0].statistics.viewCount}).catch(e=>console.log("error",e))};getSubscribers();
 
-const serverStatus=document.getElementById("serverStatus"),serverMOTD=document.getElementById("serverMOTD"),serverMax=document.getElementById("serverMax"),serverOnline=document.getElementById("serverOnline"),serverVersion=document.getElementById("serverVersion");let getServer=()=>{var e;fetch("https://eu.mc-api.net/v3/server/ping/mc.hypixel.net",{method:"GET",redirect:"follow"}).then(e=>e.json()).then(e=>{console.log(e),serverStatus.innerHTML=e.online,serverMax.innerHTML=e.players.max,serverOnline.innerHTML=e.players.online,serverVersion.innerHTML=e.version.name,serverMOTD.innerHTML=e.description}).catch(e=>console.log("error",e))};getServer();
+// const serverStatus=document.getElementById("serverStatus"),serverMOTD=document.getElementById("serverMOTD"),serverMax=document.getElementById("serverMax"),serverOnline=document.getElementById("serverOnline"),serverVersion=document.getElementById("serverVersion");let getServer=()=>{var e;fetch("https://eu.mc-api.net/v3/server/ping/mc.hypixel.net",{method:"GET",redirect:"follow"}).then(e=>e.json()).then(e=>{console.log(e),serverStatus.innerHTML=e.online,serverMax.innerHTML=e.players.max,serverOnline.innerHTML=e.players.online,serverVersion.innerHTML=e.version.name,serverMOTD.innerHTML=e.description}).catch(e=>console.log("error",e))};getServer();
+const serverStatus = document.getElementById("serverStatus"),
+  serverMOTD = document.getElementById("serverMOTD"),
+  serverMax = document.getElementById("serverMax"),
+  serverOnline = document.getElementById("serverOnline"),
+  serverVersion = document.getElementById("serverVersion");
+let getServer = () => {
+  var e;
+  fetch("https://eu.mc-api.net/v3/server/ping/mc.hypixel.net", {
+    method: "GET",
+    redirect: "follow"
+  }).then(e => e.json()).then(e => {
+    console.log(e),
+    serverStatus.innerHTML = e.online,
+    serverMax.innerHTML = e.players.max,
+    serverOnline.innerHTML = e.players.online,
+    serverVersion.innerHTML = e.version.name,
+    serverMOTD.innerHTML = e.description.replace("§a", "")
+  }).catch(e => console.log("error", e))
+};
+getServer();
 
 const getGitSubs=document.getElementById("getGitSubs"),getGitRepo=document.getElementById("getGitRepo");let getInfo=()=>{var e;fetch("https://api.github.com/users/headbodyscript",{method:"GET",redirect:"follow"}).then(e=>e.json()).then(e=>{console.log(e),getGitSubs.innerHTML=e.followers,getGitRepo.innerHTML=e.public_repos}).catch(e=>console.log("error",e))};getInfo();
 
@@ -26,5 +46,3 @@ const README=document.getElementById("README");(async()=>{let t=await (await fet
 if (location.href.indexOf("vcard") > -1) {location.href = 'https://headbodyscript.nl/assets/me.vcf';}
 
 
-
-const server = serverMOTD.replace("§a", "");
