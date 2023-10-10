5ab8ea6594ca5d4e9b8204baf147975798ed6546
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-10-10 13:08:44 +0200

    CSS

--- HeadBodyScript/fakeindex.js
+++ HeadBodyScript/fakeindex.js
@@ -1,3 +1,4 @@
+
 let apiGitHub = () => {
   var GitRepoList;
 
@@ -6,11 +7,12 @@ let apiGitHub = () => {
     .then(GitRepoList => {
       console.log
       (GitRepoList)
+      document.body.innerHTML = categories.reduce((prev, cat) => prev + `<h2>${cat.allow_forking}</h2> <h2>${cat.allow_forking}</h2>`, '');
+
+      
 
       /* What it is */
       List.innerHTML = GitRepoList
-
-      /* Result */
       List0.innerHTML = GitRepoList[0].html_url
       List1.innerHTML = GitRepoList[1].html_url
       List2.innerHTML = GitRepoList[2].html_url
@@ -21,3 +23,8 @@ let apiGitHub = () => {
 };
 
 apiGitHub();
+
+
+
+// const categories = [{id: 1, Image: "https://i.ibb.co/CnkbTqm/download-ixid-Mnwx.jpg", Title: "Lorem", }, {id: 2, Image: "https://i.ibb.co/CnkbTqm/download-ixid.jpg", Title: "Ipsum", }, {id: 3, Image: "https://i.ibb.co/CnkbTqm/download.png", Title: "lorem", }, ];
+// document.body.innerHTML = categories.reduce((prev, cat) => prev + `<h2>${cat.Title}</h2> <img src='${cat.Image}' />`, '');
