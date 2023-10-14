718a292af6a914c30263b51b0e0272bf31856003
IHeadBodyScript <IHeadBodyScript@gmail.com>
2023-10-14 21:23:32 +0200

    Splash test

--- HeadBodyScript/assets/js/init/index.js
+++ HeadBodyScript/assets/js/init/index.js
@@ -68,3 +68,32 @@ GetGitRepo();
 (async()=>{let t=await (await fetch("https://raw.githubusercontent.com/HeadBodyScript/headbodyscript.github.io/main/README.md")).text();README0.innerHTML=t})();
 if (location.href.indexOf("vcard") > -1) {location.href = 'https://headbodyscript.nl/assets/me.vcf';};
 
+
+
+// splash
+
+let intro = document.querySelector('.splash-intro');
+let logo = document.querySelector('.splash-logo-header');
+let logoSpan = document.querySelectorAll('.splash-logo');
+
+
+window.addEventListener('DOMContentLoaded', () => {
+setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+    setTimeout(() => {
+        span.classList.add('active');
+      }, (idx + 1) * 400)
+    });
+    setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+        span.classList.remove('active');
+        span.classList.add('fade');
+        }, (idx + 1) * 50)
+    })
+    }, 2000);
+    setTimeout(() => {
+    intro.style.top = '-100vh';
+    },2300)
+})
+})
