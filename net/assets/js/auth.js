03b233882139329849ccd28ebe06a7580a53124f
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-05-24 23:38:09 +0200

    Update

---
+++ net/assets/js/auth.js
@@ -0,0 +1,50 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth, signInWithPopup, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+// import { initializeApp } from "firebase/app";
+// import { GithubAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6i1H967AaI7mmIh8DvAR_GRI3U6O3qdA",
+  authDomain: "headbodyscript.firebaseapp.com",
+  projectId: "headbodyscript",
+  storageBucket: "headbodyscript.appspot.com",
+  messagingSenderId: "561751453459",
+  appId: "1:561751453459:web:8daaf437c21f3841cff4e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GithubAuthProvider();
+
+github.addEventListener('click', githubLogin)
+function githubLogin(){
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("test")
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+    
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+}
+
