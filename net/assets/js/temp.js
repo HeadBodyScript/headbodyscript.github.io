497f9cd01e21ebc641b33092ecf03b50ee97a639
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-09-18 13:08:00 +0200

    Structure files

--- net/assets/js/temp.js
+++
@@ -1,9 +0,0 @@
-// const d = new Date().getDate();
-// console.log(d)
-
-// var insertTime = document.getElementById("insertTime")
-// insertTime.innerHTML =
-// `
-// <div style="height: fit-content; font-size: small;">7:20 PM</div>
-// <div style="height: fit-content; font-size: small;">9//2024</div>
-// `
