70a0f6967d0f337ff11df397adac52a8e86d7d9b
HeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2025-09-22 15:00:13 +0200

    update

---
+++ my-app/src/app/demo/blog/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/import/navbar'
+import Footer from '@/components/import/footer'
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      
+      <Footer/>
+    </>
+  );
+}
